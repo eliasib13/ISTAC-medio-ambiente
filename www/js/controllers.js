33a3efc87c1c37f3c96365e38da8a5d4b7e34df5
Eliasib García <eliasib.lv.12@gmail.com>
2015-04-01 13:28:10 +0100

    Creados selectores de granularidad geográfica.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,7 +39,7 @@ angular.module('starter.controllers', [])
                 jsonp: "_callback",
                 success: function(data) {
                     for (var i = 0; i < data.items.length; i++)
-                        $scope.categorias.push({id: i, name: data.items[i].id, title: data.items[i].title.es});
+                        $scope.categorias.push({id: i, code: data.items[i].id, title: data.items[i].title.es});
 
                     $ionicLoading.hide();
                 }
@@ -47,8 +47,54 @@ angular.module('starter.controllers', [])
         });
     })
 
-    .controller('CategoriaCtrl', function($stateParams, $scope) {
+    .controller('CategoriaCtrl', function($stateParams, $scope, $ionicLoading) {
+        $ionicLoading.show({
+            template: $scope.loadingTemplate
+        });
+
         $scope.categoria = $scope.categorias[$stateParams.categoriaId];
+
+        $scope.geoDimensions = [];
+        $scope.lugares = [];
+
+        $scope.updateGeoDim = function(val) {
+            $scope.geoDimVal = val;
+            $scope.lugares = [];
+
+            var index = 0;
+            for (var i = 0; i < $scope.datos_consulta.dimension.GEOGRAPHICAL.representation.length; i++) {
+                if ($scope.datos_consulta.dimension.GEOGRAPHICAL.representation[i].granularityCode == $scope.geoDimVal.code) {
+                    $scope.lugares.push(
+                        {
+                            id: index,
+                            code: $scope.datos_consulta.dimension.GEOGRAPHICAL.representation[i].code,
+                            title: $scope.datos_consulta.dimension.GEOGRAPHICAL.representation[i].title.es
+                        });
+                    index++;
+                }
+            }
+        };
+
+
+        $scope.updateLugar = function(val) {
+            console.log('update lugar: '+JSON.stringify(val));
+        }
+
+        $scope.$on('$ionicView.afterEnter', function() {
+            $.ajax({
+                type: "GET",
+                url: "http://www.gobiernodecanarias.org/istac/indicators/api/indicators/v1.0/indicators/"+$scope.categoria.code.toUpperCase()+"?api_key=special-key",
+                dataType: "jsonp",
+                jsonp: "_callback",
+                success: function(data) {
+                    $scope.datos_consulta = data;
+
+                    for (var i = 0; i < $scope.datos_consulta.dimension.GEOGRAPHICAL.granularity.length; i++)
+                        $scope.geoDimensions.push({id: i, code: $scope.datos_consulta.dimension.GEOGRAPHICAL.granularity[i].code, title: $scope.datos_consulta.dimension.GEOGRAPHICAL.granularity[i].title.es});
+                    $ionicLoading.hide();
+                }
+            });
+        });
     })
 
     .controller('AboutCtrl', function($cordovaInAppBrowser) {

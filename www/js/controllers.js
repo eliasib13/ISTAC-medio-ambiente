c4fcfc219419ed8dfaaed4665b98c1ff8dd7c4c6
Eliasib García <eliasib.lv.12@gmail.com>
2015-06-18 17:56:19 +0100

    Código refactorizado y preparado para comenzar el desarrollo de la obtención de los datos para los indicadores derivados.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -323,6 +323,89 @@ angular.module('starter.controllers', [])
             $scope.closeModal();
         }
 
+        $scope.getDatoIndex = function (geoCode, timeCode) {
+            var geoIndex, timeIndex, measureIndex;
+            geoIndex = $scope.result_consulta.dimension.GEOGRAPHICAL.representation.index[geoCode];
+            timeIndex = $scope.result_consulta.dimension.TIME.representation.index[timeCode];
+            measureIndex = $scope.result_consulta.dimension.MEASURE.representation.index['ABSOLUTE'];
+
+            var geoSize, timeSize, measureSize;
+            geoSize = $scope.result_consulta.dimension.GEOGRAPHICAL.representation.size;
+            timeSize = $scope.result_consulta.dimension.TIME.representation.size;
+            measureSize = $scope.result_consulta.dimension.MEASURE.representation.size;
+
+            return (geoIndex * timeSize * measureSize) + (timeIndex * measureSize) + measureIndex;
+        };
+
+        $scope.drawChart = function(){
+            for (var i = 0; i < $scope.selectedLugares.length; i++) {
+                var data = [];
+                for (var k = 0; k < $scope.selectedTiempos.length; k++) {
+                    data.push({
+                        "dato": $scope.result_consulta.observation[$scope.getDatoIndex($scope.selectedLugares[i].code, $scope.selectedTiempos[k].code)],
+                        "fecha": $scope.selectedTiempos[k].code
+                    });
+                }
+                data.reverse();
+
+                $('#graf-' + i).empty();
+                var grafica = d3.select('#graf-' + i),
+                    WIDTH = $('#graf-' + i).width(),
+                    HEIGHT = $('#graf-' + i).height(),
+                    MARGINS = {
+                        top: 20,
+                        right: 20,
+                        bottom: 20,
+                        left: 50
+                    },
+                    xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([parseInt(data[0].fecha),parseInt(data[data.length-1].fecha)]),
+                    maxDato = -Infinity, minDato = Infinity;
+
+                for (var x = 0; x < data.length; x++) {
+                    var dato_int = parseFloat(data[x].dato)
+                    if (dato_int > maxDato)
+                        maxDato = dato_int;
+                    if (dato_int < minDato)
+                        minDato = dato_int;
+                }
+                var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([minDato,maxDato]),
+                    xAxis = d3.svg.axis().scale(xScale).tickFormat(d3.format("d")).ticks(6),
+                    yAxis = d3.svg.axis().scale(yScale).orient("left").tickFormat(d3.format("d")).ticks(6);
+
+                grafica.append("svg:g").attr("class", "axis").attr("transform", "translate(0, " + (HEIGHT - MARGINS.bottom) + ")").call(xAxis);
+                grafica.append("svg:g").attr("class", "axis").attr("transform", "translate(" + (MARGINS.left) + ",0)").call(yAxis);
+
+                var lineGen = d3.svg.line()
+                    .x(function (d) {
+                        return xScale(d.fecha);
+                    })
+                    .y(function (d) {
+                        return yScale(d.dato);
+                    });
+
+                grafica.append('svg:path')
+                    .attr('d', lineGen(data))
+                    .attr('stroke', '#0c63ee')
+                    .attr('stroke-width', 2)
+                    .attr('fill', 'none');
+            }
+        };
+
+        $scope.data_mode = 1;
+
+        $scope.setDataMode = function (mode) {
+            $scope.data_mode = mode;
+
+            if(mode == 2){ // Mostrar gráficas
+                setTimeout($scope.drawChart, 0);
+            }
+        }
+
+        $scope.isDataMode = function (mode) {
+            return $scope.data_mode == mode;
+        }
+
+
         $('#consultar').click(function(e){
             $ionicLoading.show({
                 template: $scope.loadingTemplate
@@ -333,151 +416,74 @@ angular.module('starter.controllers', [])
             $scope.selectedLugares = [];
             $scope.selectedTiempos = [];
 
-            var base_url = 'http://www.gobiernodecanarias.org/istac/indicators/api/indicators/v1.0/indicators/' + $stateParams['indicadorId'].toUpperCase() + '/data?';
-            var representation = 'representation=';
-            var granularity = 'granularity=';
-            var geographical = 'GEOGRAPHICAL';
-            var time = 'TIME';
-            var measure = 'MEASURE';
-            var end_url = '&api_key=special-key';
+            if (!$scope.indicadoresDerivados[$scope.categoriaCode][$scope.indicadorCode]) { // Si es un indicador básico...
+                var base_url = 'http://www.gobiernodecanarias.org/istac/indicators/api/indicators/v1.0/indicators/' + $stateParams['indicadorId'].toUpperCase() + '/data?';
+                var representation = 'representation=';
+                var granularity = 'granularity=';
+                var geographical = 'GEOGRAPHICAL';
+                var time = 'TIME';
+                var measure = 'MEASURE';
+                var end_url = '&api_key=special-key';
 
-            var url_consulta = base_url+representation+geographical+'['+$scope.canaryCode+'|';
+                var url_consulta = base_url + representation + geographical + '[' + $scope.canaryCode + '|';
 
-            for(var i = 0; i < $scope.lugares.length; i++) {
-                if ($scope.lugares[i].isSelected) {
-                    url_consulta += $scope.lugares[i].code + '|';
-                    $scope.selectedLugares.push($scope.lugares[i]);
+                for (var i = 0; i < $scope.lugares.length; i++) {
+                    if ($scope.lugares[i].isSelected) {
+                        url_consulta += $scope.lugares[i].code + '|';
+                        $scope.selectedLugares.push($scope.lugares[i]);
+                    }
                 }
-            }
 
-            if ($scope.selectedLugares.length == 0) {
-                $scope.selectedLugares = $scope.lugares;
-                for(var i = 0; i < $scope.selectedLugares.length; i++)
-                    url_consulta += $scope.selectedLugares[i].code + '|';
-            }
-
-            url_consulta += ']:' + time + '[';
-
-            for(var i = 0; i < $scope.tiempos.length; i++) {
-                if ($scope.tiempos[i].isSelected) {
-                    url_consulta += $scope.tiempos[i].code + '|';
-                    $scope.selectedTiempos.push($scope.tiempos[i]);
+                if ($scope.selectedLugares.length == 0) {
+                    $scope.selectedLugares = $scope.lugares;
+                    for (var i = 0; i < $scope.selectedLugares.length; i++)
+                        url_consulta += $scope.selectedLugares[i].code + '|';
                 }
-            }
 
-            if ($scope.selectedTiempos.length == 0)
-                $scope.selectedTiempos = $scope.tiempos;
+                url_consulta += ']:' + time + '[';
 
-            url_consulta += ']:' + measure + '[ABSOLUTE]';
-            url_consulta += ']&' + granularity + geographical + '[REGIONS|ISLANDS]';
-            url_consulta += ':' + time + '[YEARLY]' + end_url;
+                for (var i = 0; i < $scope.tiempos.length; i++) {
+                    if ($scope.tiempos[i].isSelected) {
+                        url_consulta += $scope.tiempos[i].code + '|';
+                        $scope.selectedTiempos.push($scope.tiempos[i]);
+                    }
+                }
 
-            $.ajax({
-                type: "GET",
-                url: url_consulta,
-                dataType: "jsonp",
-                jsonp: "_callback",
-                timeout: 15000,
-                success: function(data) {
-                    $scope.result_consulta = data;
+                if ($scope.selectedTiempos.length == 0)
+                    $scope.selectedTiempos = $scope.tiempos;
 
-                    $scope.getDatoIndex = function (geoCode, timeCode) {
-                        var geoIndex, timeIndex, measureIndex;
-                        geoIndex = $scope.result_consulta.dimension.GEOGRAPHICAL.representation.index[geoCode];
-                        timeIndex = $scope.result_consulta.dimension.TIME.representation.index[timeCode];
-                        measureIndex = $scope.result_consulta.dimension.MEASURE.representation.index['ABSOLUTE'];
+                url_consulta += ']:' + measure + '[ABSOLUTE]';
+                url_consulta += ']&' + granularity + geographical + '[REGIONS|ISLANDS]';
+                url_consulta += ':' + time + '[YEARLY]' + end_url;
 
-                        var geoSize, timeSize, measureSize;
-                        geoSize = $scope.result_consulta.dimension.GEOGRAPHICAL.representation.size;
-                        timeSize = $scope.result_consulta.dimension.TIME.representation.size;
-                        measureSize = $scope.result_consulta.dimension.MEASURE.representation.size;
+                $.ajax({
+                    type: "GET",
+                    url: url_consulta,
+                    dataType: "jsonp",
+                    jsonp: "_callback",
+                    timeout: 15000,
+                    success: function (data) {
+                        $scope.result_consulta = data;
 
-                        return (geoIndex * timeSize * measureSize) + (timeIndex * measureSize) + measureIndex;
-                    };
+                        $scope.openModal();
 
-                    $scope.openModal();
+                        $(window).resize($scope.drawChart);
 
-                    $scope.drawChart = function(){
-                        for (var i = 0; i < $scope.selectedLugares.length; i++) {
-                            var data = [];
-                            for (var k = 0; k < $scope.selectedTiempos.length; k++) {
-                                data.push({
-                                    "dato": $scope.result_consulta.observation[$scope.getDatoIndex($scope.selectedLugares[i].code, $scope.selectedTiempos[k].code)],
-                                    "fecha": $scope.selectedTiempos[k].code
-                                });
-                            }
-                            data.reverse();
-
-                            $('#graf-' + i).empty();
-                            var grafica = d3.select('#graf-' + i),
-                                WIDTH = $('#graf-' + i).width(),
-                                HEIGHT = $('#graf-' + i).height(),
-                                MARGINS = {
-                                    top: 20,
-                                    right: 20,
-                                    bottom: 20,
-                                    left: 50
-                                },
-                                xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([parseInt(data[0].fecha),parseInt(data[data.length-1].fecha)]),
-                                maxDato = -Infinity, minDato = Infinity;
-
-                            for (var x = 0; x < data.length; x++) {
-                                var dato_int = parseFloat(data[x].dato)
-                                if (dato_int > maxDato)
-                                    maxDato = dato_int;
-                                if (dato_int < minDato)
-                                    minDato = dato_int;
-                            }
-                            var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([minDato,maxDato]),
-                                xAxis = d3.svg.axis().scale(xScale).tickFormat(d3.format("d")).ticks(6),
-                                yAxis = d3.svg.axis().scale(yScale).orient("left").tickFormat(d3.format("d")).ticks(6);
-
-                            grafica.append("svg:g").attr("class", "axis").attr("transform", "translate(0, " + (HEIGHT - MARGINS.bottom) + ")").call(xAxis);
-                            grafica.append("svg:g").attr("class", "axis").attr("transform", "translate(" + (MARGINS.left) + ",0)").call(yAxis);
-
-                            var lineGen = d3.svg.line()
-                                .x(function (d) {
-                                    return xScale(d.fecha);
-                                })
-                                .y(function (d) {
-                                    return yScale(d.dato);
-                                });
-
-                            grafica.append('svg:path')
-                                .attr('d', lineGen(data))
-                                .attr('stroke', '#0c63ee')
-                                .attr('stroke-width', 2)
-                                .attr('fill', 'none');
-                        }
-                    };
-
-                    $(window).resize($scope.drawChart);
-
-                    $scope.data_mode = 1;
-
-                    $scope.setDataMode = function (mode) {
-                        $scope.data_mode = mode;
-
-                        if(mode == 2){ // Mostrar gráficas
-                            setTimeout($scope.drawChart, 0);
+                        $ionicLoading.hide();
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        if (textStatus == "timeout") {
+                            navigator.notification.alert('La conexión con la fuente de datos ha expirado. Inténtelo de nuevo.',
+                                errorDataTimeout,
+                                'Error',
+                                'Aceptar');
                         }
                     }
-
-                    $scope.isDataMode = function (mode) {
-                        return $scope.data_mode == mode;
-                    }
-
-                    $ionicLoading.hide();
-                },
-                error: function (jqXHR, textStatus, errorThrown){
-                    if(textStatus == "timeout") {
-                        navigator.notification.alert('La conexión con la fuente de datos ha expirado. Inténtelo de nuevo.',
-                            errorDataTimeout,
-                            'Error',
-                            'Aceptar');
-                    }
-                }
-            });
+                });
+            }
+            else { // Si es un indicador derivado...
+                // TODO Obtener datos de marcadores derivados
+            }
         });
     })
 
